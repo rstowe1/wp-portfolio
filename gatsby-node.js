5098aa8518e8268e32e80d204944b5a6d3123a7f
Ryan <ryanwstowe@gmail.com>
2019-07-31 15:36:13 -0400

    wip

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,21 +3,10 @@ const Promise = require(`bluebird`)
 const path = require(`path`)
 const slash = require(`slash`)
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for WordPress pages (route : /{slug})
-// Will create pages for WordPress posts (route : /post/{slug})
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   // createRedirect({ fromPath: "/", toPath: "/home", redirectInBrowser: true, isPermanent: true })
   return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local WordPress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
-
     // ==== PAGES (WORDPRESS NATIVE) ====
     graphql(
       `
@@ -44,22 +33,11 @@ exports.createPages = ({ graphql, actions }) => {
           reject(result.errors)
         }
 
-        // Create Page pages.
         const pageTemplate = path.resolve("./src/templates/page.js")
         const portfolioUnderContentTemplate = path.resolve("./src/templates/portfolioUnderContent.js")
-        // We want to create a detailed page for each
-        // page node. We'll just use the WordPress Slug for the slug.
-        // The Page ID is prefixed with 'PAGE_'
         _.each(result.data.allWordpressPage.edges, edge => {
-          // Gatsby uses Redux to manage its internal state.
-          // Plugins and sites can use functions like "createPage"
-          // to interact with Gatsby.
 
           createPage({
-            // Each page is required to have a `path` as well
-            // as a template component. The `context` is
-            // optional but is often necessary so the template
-            // can query data specific to each page.
             path: `/${edge.node.slug}/`,
             component: slash(edge.node.template === "portfolio_under_content.php" ? portfolioUnderContentTemplate : pageTemplate),
             context: edge.node,
@@ -98,9 +76,6 @@ exports.createPages = ({ graphql, actions }) => {
             reject(result.errors)
           }
           const portfolioTemplate = path.resolve("./src/templates/portfolio.js")
-          // We want to create a detailed page for each
-          // post node. We'll just use the WordPress Slug for the slug.
-          // The Post ID is prefixed with 'POST_'
           _.each(result.data.allWordpressWpPortfolio.edges, edge => {
             createPage({
               path: `/portfolio/${edge.node.slug}/`,
