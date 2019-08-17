3238ef01adcf3cc5be7561cdc36597a4a7816189
Ryan <ryanwstowe@gmail.com>
2019-08-17 10:51:26 -0400

    work

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,52 +32,52 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-
-        baseUrl: process.env.API_URL,
-        protocol: process.env.API_PROTOCOL,
-        hostingWPCOM: false,
-        useACF: true,
-        acfOptionPageIds: [],
-        auth: {
-          htaccess_user: "your-htaccess-username",
-          htaccess_pass: "your-htaccess-password",
-          htaccess_sendImmediately: false,
-          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
-          wpcom_app_clientId: "54793",
-          wpcom_user: "gatsbyjswpexample@gmail.com",
-          wpcom_pass: process.env.WORDPRESS_PASSWORD,
-          jwt_user: process.env.JWT_USER,
-          jwt_pass: process.env.JWT_PASSWORD,
-          jwt_base_path: "/jwt-auth/v1/token",
-        },
-        cookies: {},
-        verboseOutput: false,
-        perPage: 100,
-        searchAndReplaceContentUrls: {
-          sourceUrl: "https://source-url.com",
-          replacementUrl: "https://replacement-url.com",
-        },
-        concurrentRequests: 10,
-        includedRoutes: [
-          "**/*/*/categories",
-          "**/*/*/posts",
-          "**/*/*/pages",
-          "**/*/*/media",
-          "**/*/*/tags",
-          "**/*/*/taxonomies",
-          "**/*/*/users",
-          "**/*/*/menus",
-          "**/*/*/portfolio",
-        ],
-        excludedRoutes: [],
-        normalizer: function({ entities }) {
-          return entities
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-wordpress",
+    //   options: {
+    //
+    //     baseUrl: process.env.API_URL,
+    //     protocol: process.env.API_PROTOCOL,
+    //     hostingWPCOM: false,
+    //     useACF: true,
+    //     acfOptionPageIds: [],
+    //     auth: {
+    //       htaccess_user: "your-htaccess-username",
+    //       htaccess_pass: "your-htaccess-password",
+    //       htaccess_sendImmediately: false,
+    //       wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+    //       wpcom_app_clientId: "54793",
+    //       wpcom_user: "gatsbyjswpexample@gmail.com",
+    //       wpcom_pass: process.env.WORDPRESS_PASSWORD,
+    //       jwt_user: process.env.JWT_USER,
+    //       jwt_pass: process.env.JWT_PASSWORD,
+    //       jwt_base_path: "/jwt-auth/v1/token",
+    //     },
+    //     cookies: {},
+    //     verboseOutput: false,
+    //     perPage: 100,
+    //     searchAndReplaceContentUrls: {
+    //       sourceUrl: "https://source-url.com",
+    //       replacementUrl: "https://replacement-url.com",
+    //     },
+    //     concurrentRequests: 10,
+    //     includedRoutes: [
+    //       "**/*/*/categories",
+    //       "**/*/*/posts",
+    //       "**/*/*/pages",
+    //       "**/*/*/media",
+    //       "**/*/*/tags",
+    //       "**/*/*/taxonomies",
+    //       "**/*/*/users",
+    //       "**/*/*/menus",
+    //       "**/*/*/portfolio",
+    //     ],
+    //     excludedRoutes: [],
+    //     normalizer: function({ entities }) {
+    //       return entities
+    //     },
+    //   },
+    // },
     'gatsby-plugin-netlify'
   ],
 }
