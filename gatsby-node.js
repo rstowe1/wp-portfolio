96f6962d541fb0224a42053f16bf75c17dafe205
Ryan <ryanwstowe@gmail.com>
2019-08-24 17:07:16 -0400

    a

--- gatsby-node.js
+++ gatsby-node.js
@@ -69,7 +69,7 @@ exports.createPages = ({ graphql, actions }) => {
                 }
               }
             }
-          `
+          `,
         ).then(result => {
           if (result.errors) {
             console.log(result.errors)
